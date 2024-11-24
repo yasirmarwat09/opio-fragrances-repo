2cb176ce7265c469bf38ee0fa86baaff15137e4e
Yasir <121509154+yasirmarwat09@users.noreply.github.com>
2024-11-24 23:52:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -131,17 +131,17 @@ let startX = 0;
 function updateImagesForSmallScreens() {
   if (window.innerWidth < 800) {
     heroImages.forEach((img, index) => {
-      img.src = `../Assets/${index + 1}-hero-img-resized.jpg`;
+      img.src = `Assets/${index + 1}-hero-img-resized.jpg`;
     });
     if (bottomHeroImg) {
-      bottomHeroImg.src = `../Assets/mobile-size-bottom-hero-img.jpg`;
+      bottomHeroImg.src = `Assets/mobile-size-bottom-hero-img.jpg`;
     }
   } else {
     heroImages.forEach((img, index) => {
-      img.src = `../Assets/${index + 1}-hero-img.jpg`;
+      img.src = `Assets/${index + 1}-hero-img.jpg`;
     });
     if (bottomHeroImg) {
-      bottomHeroImg.src = `../Assets/bottom-hero-img.jpg`;
+      bottomHeroImg.src = `Assets/bottom-hero-img.jpg`;
     }
   }
 }
